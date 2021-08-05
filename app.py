2540898e90b5e9d3b5ab3097a85472edcebb84fa
alonb21-meet <alonb21@meet.mit.edu>
2021-08-05 13:02:33 +0300

    easygui

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ session = DBSession()
 from flask import Flask, jsonify, request, render_template, url_for, redirect
 import random
 import requests, json
+import easygui
 
 app = Flask(  # Create a flask app
     __name__,
@@ -38,6 +39,11 @@ mail = Mail(app)
 login = False
 isAdmin = False
 email = ""
+
+
+def Mbox(title, text):
+    return easygui.msgbox(text, title)
+
 
 
 def add_new_workshop(workshop_name, details, pictures):
@@ -226,17 +232,13 @@ def news():
     global email
     global login
     global isAdmin
-    user = ''
     workshops = get_all_workshops()
-    registered = get_all_registered()
-    if email == '':
-        user = ""
-    else:
-        user = get_account(email)
     if request.method == 'POST':
         return render_template('news.html', login=login, email=email, workshops=workshops, admin=isAdmin)
     else:
         return render_template('news.html', login=login, email=email, workshops=workshops, admin=isAdmin)
+
+
 
 
 @app.route('/admin_page_page', methods=['GET'])
@@ -288,6 +290,10 @@ def register_workshop(workshop_id):
         mail.send(msg)
         user_id = get_account(email).id
         register_to_workshop(workshop_id, user_id)
+    elif show == False:
+        Mbox('Attention', 'you already signed to this workshop')
+    elif login == False:
+        Mbox('Attention', 'you are not logged in to an account')
     return redirect('/news')
 
 
